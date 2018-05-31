2b25ae96d71b4b79e92307d6548b04cbcd48233e
nathanbeier <nathan.beier@outlook.com>
2018-05-30 23:14:11 -0500

    Adding to repository prior to API conversion

---
+++ DutchTreat/Data/DutchRepository.cs
@@ -0,0 +1,47 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DutchTreat.Data
+{
+    public class DutchRepository : IDutchRepository
+    {
+        private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
+
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
+        {
+            _ctx = ctx;
+            _logger = logger;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            _logger.LogInformation("GetAllProducts was ran");
+
+            return _ctx.Products
+                .OrderBy(p => p.Title)
+                .ToList();
+        }
+
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return _ctx.Products
+                .Where(p => p.Category == category)
+                .ToList();
+        }
+
+        public bool SaveAll()
+        {
+            return _ctx.SaveChanges() > 0;
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,75 @@
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer(_config.GetConnectionString("DutchConnectionString"));
+            }
+            );
+
+            services.AddTransient<IMailService, NullMailService>();
+
+            services.AddTransient<DutchSeeder>();
+            // Support for real mail service
+
+            services.AddScoped<IDutchRepository, DutchRepository>();
+
+            services.AddMvc();
+              //.AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(cfg =>
+            {
+                cfg.MapRoute("Default",
+                  "{controller}/{action}/{id?}",
+                  new { controller = "App", Action = "Index" });
+            });
+
+           
+            if (env.IsDevelopment())
+            {
+                //Seed the databse
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    var seeder = scope.ServiceProvider.GetService<DutchSeeder>();
+                    seeder.Seed();
+                }
+            }
+
+        }
+    }
+}
